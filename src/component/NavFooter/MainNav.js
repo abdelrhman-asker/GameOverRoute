ffde10c443230d3bbae94e3ba0fbf48d34b165e3
abdelrhman-asker <abdoasker11@yahoo.com>
2023-09-19 21:11:12 +0300

    logoedits

--- src/component/NavFooter/MainNav.js
+++ src/component/NavFooter/MainNav.js
@@ -1,47 +1,44 @@
-import React, { useState } from 'react';
-import "./NavFooter.css"
-import Logo from "./images/logo.png"
-import { AiOutlineMenu } from 'react-icons/ai';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  Nav,
-  NavItem,
-} from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./NavFooter.css";
+import Logo from "../images/logo.png";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const MainNav = (args) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
   return (
-    <div className='MainNavDiv'>
-        <Navbar className='container'  expand="lg" {...args}>
-        
-            <Link to="/Home" className='LogoTextGameOver'>
-            <img alt='MLogo' width="70px" src={Logo} />
-            Game Over
-            </Link>
-        <NavbarToggler   onClick={toggle} >
-        <AiOutlineMenu />
+    <div className="MainNavDiv">
+      <Navbar className="container" expand="lg" {...args}>
+        <Link to="/Home" className="LogoTextGameOver">
+          <img alt="MLogo" width="70px" src={Logo} />
+          Game Over
+        </Link>
+        <NavbarToggler onClick={toggle}>
+          <AiOutlineMenu />
         </NavbarToggler>
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="ms-auto text-center d-flex justify-center align-center gap-4" navbar>
+          <Nav
+            className="ms-auto text-center d-flex justify-center align-center gap-4"
+            navbar
+          >
             <NavItem>
-              <Link className='LoginButn ' to="/SignIn">LogIn</Link>
+              <Link className="LoginButn " to="/SignIn">
+                LogIn
+              </Link>
             </NavItem>
             <NavItem>
-              <Link className='signupButn' to="/">
+              <Link className="signupButn" to="/">
                 Join Free
               </Link>
             </NavItem>
-           
           </Nav>
         </Collapse>
       </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default MainNav
+export default MainNav;
