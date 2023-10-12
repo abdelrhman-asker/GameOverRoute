16224de9e14650dee42701c9d513cfedc3dfe31c
abdelrhman asker <abdoasker11@yahoo.com>
2023-10-12 21:47:38 +0300

    addingpagination

--- src/component/All/All.js
+++ src/component/All/All.js
@@ -1,51 +1,103 @@
-import { Pagination } from '@mui/material'
-import axios from 'axios'
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { dataAllContext, dataContext } from '../../context/Store'
-import HomeNav from '../NavFooter/HomeNav'
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { dataAllContext, dataContext } from "../../context/Store";
+import HomeNav from "../NavFooter/HomeNav";
+import { Pagination } from "react-bootstrap";
 
 const All = () => {
-    const {dataAll} = useContext(dataContext)
+  const { dataAll } = useContext(dataContext);
 
-console.log("all", dataAll.length%20)
+  console.log("all", dataAll.length % 20);
+  console.log("dataAll", dataAll);
+
+  const itemsPerPage = 24;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = dataAll.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(dataAll.length / itemsPerPage);
+
+  const goToPage = (page) => {
+    setCurrentPage(page);
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  console.log("currentData", currentData);
+  console.log("totalPages", totalPages);
+  console.log("nextPage", nextPage);
 
   return (
-    <div >
-        
-    <HomeNav  />
-    {/* <Pagination className='text-center m-auto' count={20} color="secondary" /> */}
-<div className='MainAllDiv container-fluid  col-12 text-left mx-auto'>
-      {dataAll.map((dataAll,index, idt)=> {
-      return (
-        <>
-              <Link to={`/Details/${dataAll.id}`}  dataAll={dataAll}  className='MainDivMapImgAndTitle MainDivMapImgAndTitleAll mx-auto col-10 '  key={index.id} style={{position:"relative"}}>
-                  
-                  <div className='ImgMainDivForZoom col-xl-10  col-10' >
-                 
-
-                  <img className='col-xl-12  col-12' src={dataAll.thumbnail} />
+    <div>
+      <HomeNav />
+      {/* <Pagination className='text-center m-auto' count={20} color="secondary" /> */}
+      <div className="MainAllDiv container-fluid  col-12 text-left mx-auto">
+        {currentData.map((dataAll, index, idt) => {
+          return (
+            <>
+              <Link
+                to={`/Details/${dataAll.id}`}
+                dataAll={dataAll}
+                className="MainDivMapImgAndTitle MainDivMapImgAndTitleAll mx-auto col-10 "
+                key={index.id}
+                style={{ position: "relative" }}
+              >
+                <div className="ImgMainDivForZoom col-xl-10  col-10">
+                  <img
+                    className="col-xl-12  col-12"
+                    src={dataAll.thumbnail}
+                    alt="Thumbnail"
+                  />
                 </div>
-                <div className='py-3 col-12 d-flex text-center justify-content-center align-items-center' >
-                  <div className='col'>
-              {dataAll.title}
-              </div>
-              <div className='col'>
-                <span>FREE</span>
-               {/* { console.log("my idt" ,idt)} */}
-              </div>
-              </div> 
-              
-{/* {              localStorage.setItem('dataAllas', dataAll.id)
+                <div className="py-3 col-12 d-flex text-center justify-content-center align-items-center">
+                  <div className="col">{dataAll.title}</div>
+                  <div className="col">
+                    <span>FREE</span>
+                    {/* { console.log("my idt" ,idt)} */}
+                  </div>
+                </div>
+
+                {/* {              localStorage.setItem('dataAllas', dataAll.id)
 } */}
               </Link>
-            
-              </>   )}
-              )}
-   
-              </div>
-    </div>
-  )
-}
+            </>
+          );
+        })}
 
-export default All
+        {/* Pagination controls */}
+        <div>
+          <button className="PrevBut" onClick={prevPage}>
+            {"<"} Previous
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              className={currentPage === index + 1 ? "active" : ""}
+              key={index}
+              onClick={() => goToPage(index + 1)}
+              style={{ margin: "2px" }}
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button className="NextBut" onClick={nextPage}>
+            Next {">"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default All;
--- src/component/Categories/Social.js
+++ src/component/Categories/Social.js
@@ -1,50 +1,55 @@
-import axios from 'axios'
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { BounceLoader } from 'react-spinners'
-import { datasocialContext, dataContext } from '../../context/Store'
-import HomeNav from '../NavFooter/HomeNav'
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { BounceLoader } from "react-spinners";
+import { datasocialContext, dataContext } from "../../context/Store";
+import HomeNav from "../NavFooter/HomeNav";
 
 const Social = () => {
-    const {datasocial} = useContext(dataContext)
+  const { datasocial } = useContext(dataContext);
 
-// console.log("all", datasocial)
+  // console.log("all", datasocial)
 
   return (
-    <div >
-        
-    <HomeNav  />
-    { datasocial.length > 0 ?
-
-<div className='MainAllDiv container-fluid  col-12 text-left mx-auto'>
-      {datasocial.map((datasocial,index, idt)=> {
-      return (
-      
-              <Link to={`/Details/${datasocial.id}`}  datasocial={datasocial}  className='MainDivMapImgAndTitle MainDivMapImgAndTitleAll mx-auto col-10 '  key={index.id} style={{position:"relative"}}>
-                  <div className='ImgMainDivForZoom col-xl-10  col-10' >
-                  <img className='col-xl-12  col-12' src={datasocial.thumbnail} />
+    <div>
+      <HomeNav />
+      {datasocial.length > 0 ? (
+        <div className="MainAllDiv container-fluid  col-12 text-left mx-auto">
+          {datasocial.map((datasocial, index, idt) => {
+            return (
+              <Link
+                to={`/Details/${datasocial.id}`}
+                datasocial={datasocial}
+                className="MainDivMapImgAndTitle MainDivMapImgAndTitleAll mx-auto col-10 "
+                key={index.id}
+                style={{ position: "relative" }}
+              >
+                <div className="ImgMainDivForZoom col-xl-10  col-10">
+                  <img
+                    className="col-xl-12  col-12"
+                    src={datasocial.thumbnail}
+                  />
                 </div>
-                <div className='py-3 col-12 d-flex text-center justify-content-center align-items-center' >
-                  <div className='col'>
-              {datasocial.title}
-              </div>
-              <div className='col'>
-                <span>FREE</span>
-              </div>
-              </div> 
-              
-{/* {              localStorage.setItem('datasocialas', datasocial.id)
+                <div className="py-3 col-12 d-flex text-center justify-content-center align-items-center">
+                  <div className="col">{datasocial.title}</div>
+                  <div className="col">
+                    <span>FREE</span>
+                  </div>
+                </div>
+
+                {/* {              localStorage.setItem('datasocialas', datasocial.id)
 } */}
               </Link>
-            
-              )}
-              )}
-   
-              </div>
-              : <div className='Loader'><BounceLoader color="#36d7b7" /> </div>
-            }
+            );
+          })}
+        </div>
+      ) : (
+        <div className="Loader">
+          <BounceLoader color="#36d7b7" />{" "}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
